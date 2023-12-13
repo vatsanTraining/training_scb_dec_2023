8a60c2d8a4c8ffc7dc91124c7f2b5aff7a810822
srivatsan <srivatsan@192.168.1.3>
2023-12-13 11:41:34 +0530

    Repository and Service Added

---
+++ loan_application_service/src/main/java/com/example/demo/service/LoanApplicationService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.LoanApplication;
+import com.example.demo.repos.LoanApplicationRepository;
+
+@Service
+public class LoanApplicationService {
+
+	
+	
+	 private LoanApplicationRepository repo;
+
+	 //Repository is autowired here => Constructor DI
+	public LoanApplicationService(LoanApplicationRepository repo) {
+		super();
+		this.repo = repo;
+		
+		System.out.println(this.repo.getClass().getName());
+	}
+	 
+	 
+	 
+	public List<LoanApplication> findAll(){
+		
+		return this.repo.findAll();
+	}
+	
+	public LoanApplication save(LoanApplication entity) {
+		return this.repo.save(entity);
+	}
+	
+}
