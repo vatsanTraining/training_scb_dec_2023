df597ad35834d6813c7fa8ff35443ed6fddf2670
srivatsan <srivatsan@192.168.1.3>
2023-12-13 12:00:19 +0530

    Command Line Runner Added

--- loan_application_service/src/main/java/com/example/demo/LoanApplicationServiceApplication.java
+++ loan_application_service/src/main/java/com/example/demo/LoanApplicationServiceApplication.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.LoanApplication;
+import com.example.demo.repos.LoanApplicationRepository;
 
 @SpringBootApplication
 public class LoanApplicationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanApplicationServiceApplication.class, args);
+	}
+	
+	
+	
+	@Bean
+	public CommandLineRunner runner() {
+		
+		return new CommandLineRunner() {
+			
+			
+			@Autowired
+			LoanApplicationRepository repo;
+			
+			@Override
+			public void run(String... args) throws Exception {
+				
+				repo.save(new LoanApplication(1010, "Ramesh", 200000, "TNAB2010"));
+			}
+		};
 	}
 
 }
