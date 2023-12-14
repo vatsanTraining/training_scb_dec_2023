cb8318a060bc877a53c1b178dbbe3620c4329797
srivatsan <srivatsan@192.168.1.2>
2023-12-14 09:15:18 +0530

    Repository Modified with Custom query

--- loan_application_service/src/main/java/com/example/demo/repos/LoanApplicationRepository.java
+++ loan_application_service/src/main/java/com/example/demo/repos/LoanApplicationRepository.java
@@ -1,9 +1,23 @@
 package com.example.demo.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.entity.LoanApplication;
 
 public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Integer> {
 
+	// select * from Loan application where applicantName =qryString
+	List<LoanApplication> findByApplicantName(String qryString);
+	
+	//select * from loan_application where loan_amount>qryValue // native query
+	
+	//JPQL uses the class Name and PropertyName
+	
+	@Query(value = "from LoanApplication where loanAmount>:qryValue")
+	List<LoanApplication> getGrtThanLoanAmount(double qryValue);
+	
+	
 }
