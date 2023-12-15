5e83c6b9dbfbc29b3266ff595dbcc326cb03b77f
srivatsan <srivatsan@192.168.1.3>
2023-12-15 11:55:36 +0530

    More Test Cases Added

--- loan_application_service/src/test/java/com/example/demo/LoanApplicationServiceApplicationTests.java
+++ loan_application_service/src/test/java/com/example/demo/LoanApplicationServiceApplicationTests.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import io.restassured.*;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
+
+import static org.hamcrest.CoreMatchers.*;
 @SpringBootTest
 class LoanApplicationServiceApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
@@ -27,4 +31,26 @@ class LoanApplicationServiceApplicationTests {
 		 .get("http://localhost:4040/api/v1/loans").then().statusCode(200);
 
 	}
+	
+	@Test
+	void testForGivenNameInResponse() {
+		
+		 given().auth().basic("india", "india").pathParam("id", 1010)
+		 .when()
+		 .expect().body("applicantName", equalTo("Ramesh"))
+		 .when().get("http://localhost:4040/api/v1/loans/{id}");
+
+	}
+	
+	@Test
+	void testContentType() {
+
+		             given().auth().basic("india", "india")
+	        		.when()
+			.get("http://localhost:4040/api/v1/loans")
+			.then()
+			.contentType("application/json");
+
+	}
+
 }
