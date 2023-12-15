c2a4efdcf51e00a602217ef14b20fb29952a824e
srivatsan <srivatsan@192.168.1.3>
2023-12-15 11:40:55 +0530

    RestAssured Tests Added

--- loan_application_service/src/test/java/com/example/demo/LoanApplicationServiceApplicationTests.java
+++ loan_application_service/src/test/java/com/example/demo/LoanApplicationServiceApplicationTests.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import io.restassured.*;
+import io.restassured.response.Response;
 @SpringBootTest
 class LoanApplicationServiceApplicationTests {
 
@@ -10,4 +11,11 @@ class LoanApplicationServiceApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testForStatusCode() {
+		
+		Response response = RestAssured.get("http://localhost:4040/api/v2/loans");
+	
+		response.then().statusCode(200);
+	}
 }
